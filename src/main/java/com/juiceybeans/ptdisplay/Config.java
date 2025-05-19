cb049966c0175e174a5a3b292e23b2be8ac9bf50
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2025-05-19 13:06:23 +0100

    Change Color config to String

--- src/main/java/com/juiceybeans/ptdisplay/Config.java
+++ src/main/java/com/juiceybeans/ptdisplay/Config.java
@@ -5,6 +5,8 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
+import java.awt.Color;
+
 @EventBusSubscriber(modid = Main.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
@@ -17,9 +19,9 @@ public class Config {
             BUILDER.comment("Y position of overlay (default: 4)")
                     .define("y_pos", 4);
 
-    private static final ModConfigSpec.ConfigValue<Integer> COLOR =
+    private static final ModConfigSpec.ConfigValue<String> COLOR =
             BUILDER.comment("Color of overlay text (default: 0xFFFFFF)")
-                    .define("color", 0xFFFFFF);
+                    .define("color", "0xFFFFFF");
 
     private static final ModConfigSpec.ConfigValue<Boolean> DROP_SHADOW =
             BUILDER.comment("Drop shadow for overlay text (default: true)")
@@ -41,7 +43,7 @@ public class Config {
     static void onLoad(final ModConfigEvent event) {
         xPos = X_POS.get();
         yPos = Y_POS.get();
-        color = COLOR.get();
+        color = Color.decode(COLOR.get()).getRGB();
         dropShadow = DROP_SHADOW.get();
         useStatFormat = USE_STAT_FORMAT.get();
     }
